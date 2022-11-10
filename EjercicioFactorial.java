052a2d181498d9cd464f173b25190a5b50a1bb23
Jorge <jcorvil600@g.educaand.es>
2022-11-10 12:37:05 +0100

    Update EjercicioFactorial.java

--- EjercicioFactorial.java
+++ EjercicioFactorial.java
@@ -1,12 +1,13 @@
 public class EjercicioFactorial {
+    //Calcular el factorial de un número, en este caso 4 (4*3*2*1=24)
     public static void main(String[] args){
         int num = 4;
-        int resultado = 1;
+        int factorial = 1;
         System.out.println("Calcular factorial del numero: " + num);
 
         for(int i= num; i>=1; i--){
-            resultado = i *(resultado);
+            factorial = i *(factorial);
         }
-        System.out.println("Factorial: " + resultado);
+        System.out.println("Factorial: " + factorial);
     }
 }
