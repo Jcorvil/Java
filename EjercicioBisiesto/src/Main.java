14256c8c555e449861b36bf028ec2b0d293365f3
Jorge <jcorvil600@g.educaand.es>
2022-11-11 13:56:00 +0100

    Intellij

---
+++ EjercicioBisiesto/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    /*Crear un programa que te diga si un año es bisiesto o no.
+    * Es bisiesto si es divisible entre 4 y no divisible entre 100, y también si es divisible entre 400.*/
+    public static void main(String[] args) {
+        
+    }
+}
